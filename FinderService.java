66c61f661f0c0eda9a12af7133f6f297b2779507
couroumah <ousmane.couroumah@gmail.com>
2024-11-14 09:13:48 +1100

    Update FinderService.java

--- FinderService.java
+++ FinderService.java
@@ -1,35 +1,37 @@
 package com.devops.toolbox.finder;
 
-import com.devops.toolbox.config.Settings;
-import com.devops.toolbox.util.CmfTemplatesHelper;
-import com.devops.toolbox.util.Helper;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.filefilter.IOFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.apache.commons.io.filefilter.WildcardFileFilter;
-import org.apache.logging.log4j.util.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.MessageFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import static com.devops.toolbox.util.Messages.getString;
 import static com.devops.toolbox.util.StandardConstants.*;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor
 public class FinderService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FinderService.class);
 
-    private final Settings settings;
-    private final Helper helper;
+    private final FinderEntityRepository repository;
+    private final FinderEntityMapper mapper;
 
     /**
      * Returns <code>a map<Filename,List of patterns></code>
@@ -39,38 +41,45 @@ public class FinderService {
      */
     public Map<String, List<Map<String, List<String>>>> findPatternsInFile(Path pathInputFile,
                                                               List<String> patterns){
+        Map<String, List<Map<String, List<String>>>> rc = new HashMap<>();
         try {
-            return helper.findPatternsInFile(pathInputFile, patterns);
+            return FinderHelper.findPatternsInFile(pathInputFile, patterns, Charset.defaultCharset());
         } catch (IOException e) {
-            throw new RuntimeException(
-                    MessageFormat.format("Cannot find patterns in file [{0}]. IOException : {1}",
-                            pathInputFile.getFileName(),
-                            e.getMessage()));
+//            throw new RuntimeException(
+//                    MessageFormat.format("[findPatternsInFile] - Cannot find patterns in file [{0}]. IOException : {1}",
+//                            pathInputFile,
+//                            e.getMessage()));
+            LOGGER.error(MessageFormat.format("[findPatternsInFile] - Cannot find patterns in file [{0}]. IOException : {1}",
+                    pathInputFile,
+                    e.getMessage()));
+            return rc;
         }
     }
 
     /**
      * Returns <code>a map<Filename,List of patterns></code>
+     *
      * @param pathInputDirectory directory of files to process
-     * @param patterns list of patterns to search
+     * @param patterns           list of patterns to search
+     * @param fileFilterWildCard
      * @return <code>a map<Filename,List of patterns></code>
      */
     public List<Map<String, List<Map<String, List<String>>>>> findPatternsInFiles(Path pathInputDirectory,
-                                                              List<String> patterns){
-        log.debug("--start [{}] ...", Thread.currentThread().getStackTrace()[1].getMethodName());
+                                                                                  List<String> patterns,
+                                                                                  String fileFilterWildCard){
+        LOGGER.debug("--start [{}] ...", Thread.currentThread().getStackTrace()[1].getMethodName());
 
         List<Map<String, List<Map<String, List<String>>>>> listFilesWithMatchingPatterns = new ArrayList<>();
         IOFileFilter filter;
-        if (StringUtils.hasText(settings.getFileFilterWildCard()))
-            filter = new WildcardFileFilter(settings.getFileFilterWildCard());
+        if (StringUtils.hasText(fileFilterWildCard))
+            filter = new WildcardFileFilter(fileFilterWildCard);
         else filter = TrueFileFilter.INSTANCE;
 
         List<File> listChildrenFiles = (List<File>) FileUtils.listFiles(pathInputDirectory.toFile(), filter, TrueFileFilter.INSTANCE);
 
-        Map<String, Collection<String>> map = new HashMap<>();
         listChildrenFiles.forEach(file -> listFilesWithMatchingPatterns.add(findPatternsInFile(file.toPath(), patterns)));
 
-        log.debug("--end [{}]", Thread.currentThread().getStackTrace()[1].getMethodName());
+        LOGGER.debug("--end [{}]", Thread.currentThread().getStackTrace()[1].getMethodName());
         return listFilesWithMatchingPatterns;
     }
 
@@ -81,67 +90,81 @@ public class FinderService {
      * @param patterns list of patterns
      * @return a list of tradingBankingBookReport objects
      */
-    public List<TradingBankingBookReport> getTradingBankingBookReport(String businessObject,
-                                                                      Path pathInputDirectory,
-                                                                      List<String> patterns){
-        log.debug("--start [{}] ...", Thread.currentThread().getStackTrace()[1].getMethodName());
+    public List<FinderEntityDto> getFinderEntityDtos(String businessObject,
+                                                     Path pathInputDirectory,
+                                                     List<String> patterns,
+                                                     Map<String, String> mapLookupValues,
+                                                     String fileFilterWildCard){
+        LOGGER.debug("--start [{}] ...", Thread.currentThread().getStackTrace()[1].getMethodName());
 
         List<TradingBankingBookReport> tradingBankingBookReports = new ArrayList<>();
-        List<Map<String, List<Map<String, List<String>>>>> mapList = findPatternsInFiles(pathInputDirectory, patterns);
-        Map<String, Collection<String>> mapFindPatternsInFiles = new HashMap<>();
-        Map<String, String> mapPortfolios = settings.getMapPortfolios();
+        List<FinderEntityDto> finderEntityDtos = new ArrayList<>();
+
+        List<Map<String, List<Map<String, List<String>>>>> mapList = findPatternsInFiles(pathInputDirectory, patterns, fileFilterWildCard);
 
         for (Map<String, List<Map<String, List<String>>>> map: mapList) {
             for(String key: map.keySet()){
-                log.debug("processing file [{}]", key);
+                LOGGER.debug("processing file [{}]", key);
                 List<Map<String, List<String>>> mapSubList = map.get(key);
 
                 for (Map<String, List<String>> mapMatchedLines: mapSubList){
 
                     for(String pattern: mapMatchedLines.keySet()){
 
-                        TradingBankingBookReport tradingBankingBookReport = getTradingBankingBookReport(businessObject, key, pattern, mapPortfolios, mapMatchedLines);
-                        tradingBankingBookReports.add(tradingBankingBookReport);
+                        FinderEntity finderEntity = getFinderEntity(businessObject, key, pattern, mapLookupValues, mapMatchedLines);
+                        repository.save(finderEntity);
+                        LOGGER.info("Persisted FinderEntity [{}]", finderEntity);
+                        finderEntityDtos.add(mapper.toDto(finderEntity));
+
+//                        repository.findAll().stream().forEach(finderEntityFromDB -> {
+//                            LOGGER.info("FinderEntity id[{}]", finderEntityFromDB.getID());
+//                            List<FinderEntityBody> finderMatchedBodies = finderEntityFromDB.getFinderEntityBodies();
+//                            finderMatchedBodies.stream()
+//                                    .forEach(finderEntityBody -> {
+//                                        String dataAsString = new String(finderEntityBody.getData(), StandardCharsets.UTF_8);
+//                                        LOGGER.info("--finderMatchedLine id[{}] - text [{}]", finderEntityBody.getID(), dataAsString);
+//                                    });
+//                        });
 
                     }
                 }
             }
 
         }
-        log.debug("--end [{}]", Thread.currentThread().getStackTrace()[1].getMethodName());
-        return tradingBankingBookReports;
+        LOGGER.debug("--end [{}]", Thread.currentThread().getStackTrace()[1].getMethodName());
+        return finderEntityDtos;
     }
 
-    /**
-     * Returns a TradingBankingBookReport object
-     *
-     * @param businessObject
-     * @param key
-     * @param pattern
-     * @param mapPortfolios
-     * @param mapMatchedLines
-     * @return a TradingBankingBookReport object
-     */
-    private TradingBankingBookReport getTradingBankingBookReport(String businessObject,
-                                                                 String key,
-                                                                 String pattern,
-                                                                 Map<String, String> mapPortfolios,
-                                                                 Map<String, List<String>> mapMatchedLines) {
-        return TradingBankingBookReport.builder()
+    private FinderEntity getFinderEntity(String businessObject,
+                                         String key,
+                                         String pattern,
+                                         Map<String, String> mapLookupValues,
+                                         Map<String, List<String>> mapMatchedLines){
+
+        List<String> listOfMatchedLines = mapMatchedLines.get(pattern);
+
+        FinderEntity finderEntity = FinderEntity.builder()
                 .businessObject(businessObject)
                 .shortName(getShortname(key).toUpperCase())
                 .nbInstances(mapMatchedLines.get(pattern).size())
-                .mapMatchedLines(mapMatchedLines)
                 .foundPatterns(pattern)
-                .sourcePortfolio(pattern)
-                .targetPortfolio(mapPortfolios.get(pattern))
-                .item(Strings.EMPTY)
-                .status("In Progress")
-                .action("Analyzing")
-                .comments(Strings.EMPTY)
+                .sourcePattern(pattern)
+                .targetPattern(mapLookupValues.get(pattern))
                 .absolutePath(key)
+                .action(getString("FinderService.finderEntity.action"))
+                .item(getString("FinderService.finderEntity.item"))
+                .status(getString("FinderService.finderEntity.status"))
+                .comments(getString("FinderService.finderEntity.comments"))
                 .build();
+
+        listOfMatchedLines.stream()
+                .forEach(matchedLine -> {
+                    finderEntity.addFinderEntityBody(new FinderEntityBody(matchedLine.getBytes()));
+                });
+
+        return finderEntity;
     }
+
 
     /**
      * Writes a report for a single business object having its configuration files within a specific directory
@@ -151,47 +174,70 @@ public class FinderService {
      * @param patterns           the patterns to search
      * @param pathOutputFile     the report output file
      */
-    public void writeReportFoundPatterns(String businessObject,
-                                         Path pathInputDirectory,
-                                         List<String> patterns,
-                                         Path pathOutputFile) {
-        log.info("--start [{}] businessObject [{}]...", Thread.currentThread().getStackTrace()[1].getMethodName(), businessObject);
+    private List<FinderEntityDto> doWriteReportFoundPatterns(String businessObject,
+                                            Path pathInputDirectory,
+                                            List<String> patterns,
+                                            Path pathOutputFile,
+                                            Map<String, String> mapLookupValues,
+                                            String fileFilterWildCard) {
+        LOGGER.info("--start [{}] businessObject [{}]...", Thread.currentThread().getStackTrace()[1].getMethodName(), businessObject);
 
-        List<TradingBankingBookReport> tradingBankingBookReports = getTradingBankingBookReport( businessObject,
-                                                                                                pathInputDirectory,
-                                                                                                patterns);
-        if(tradingBankingBookReports.isEmpty()){
-            return;
+        List<FinderEntityDto> finderEntityDtos = getFinderEntityDtos(
+                                                                    businessObject,
+                                                                    pathInputDirectory,
+                                                                    patterns,
+                                                                    mapLookupValues,
+                                                                    fileFilterWildCard);
+        if(finderEntityDtos.isEmpty()){
+            LOGGER.info("--no match found");
+            LOGGER.info("--end [{}] businessObject [{}]", Thread.currentThread().getStackTrace()[1].getMethodName(), businessObject);
+            return new ArrayList<>();
         }
 
-        CmfTemplatesHelper.writeToExcel(tradingBankingBookReports, pathOutputFile);
-        log.info("--end [{}] businessObject [{}]", Thread.currentThread().getStackTrace()[1].getMethodName(), businessObject);
+        FinderHelper.writeToExcel(finderEntityDtos, pathOutputFile);
+        LOGGER.info("--end [{}] businessObject [{}]", Thread.currentThread().getStackTrace()[1].getMethodName(), businessObject);
+        return finderEntityDtos;
     }
-
-
 
     /**
      * Writes a report for each business object having its configuration files within a specific directory
-     * @param mapFindPatterns a map<BusinessObject name, Path to related configuration files>
+     * @param finderSettings finder settings
      */
-    public void writeReportFoundPatterns(Map<String, String> mapFindPatterns){
-        List<String> sourcePortfolios = new ArrayList<>(settings.getMapPortfolios().keySet());
+    public List<FinderEntityDto> writeReport(FinderSettings finderSettings) {
+        List<FinderEntityDto> finderEntityDtos = new ArrayList<>();
+        Map<String,String> mapLookupValues = finderSettings.getMapLookupValues();
+        List<String> lookupValues = mapLookupValues.keySet().stream().toList();
+        Map<String,String> mapLookupDirectories = finderSettings.getMapLookupDirectories();
+        Path pathOutputDirectory = Paths.get(finderSettings.getPathOutputDirectory());
+        String fileFilterWildCard = finderSettings.getFileFilterWildCard();
 
-        mapFindPatterns.forEach((businessObject, path) -> {
+        if (!pathOutputDirectory.toFile().exists()){
+            pathOutputDirectory.toFile().mkdirs();
+        }
+
+        for (Map.Entry<String, String> entry : mapLookupDirectories.entrySet()) {
+            String businessObject = entry.getKey();
+            String path = entry.getValue();
+            Path pathLookupDirectory = Paths.get(path);
+            Path pathOutputFile = pathOutputDirectory.resolve(FinderHelper.getExportFilename(businessObject, XLSX_FILE_EXTENSION));
+
             try {
-                writeReportFoundPatterns(
-                        businessObject,
-                        Paths.get(path),
-                        sourcePortfolios,
-                        settings.getPathOutputDirectory()
-                                .resolve(settings.getExportFilename(businessObject, XLSX_FILE_EXTENSION)));
+                finderEntityDtos.addAll(
+                        doWriteReportFoundPatterns(businessObject, pathLookupDirectory, lookupValues, pathOutputFile, mapLookupValues, fileFilterWildCard)
+                );
+
             } catch (Exception e) {
-                throw new RuntimeException(
-                        MessageFormat.format("Cannot write report for businessObject [{0}]. Exception : {1}",
-                                businessObject,
-                                e.getMessage()));
+//                throw new RuntimeException(
+//                        MessageFormat.format("Cannot write report for businessObject [{0}]. Exception : {1}",
+//                                businessObject,
+//                                e.getMessage()));
+                LOGGER.error(MessageFormat.format("Cannot write report for businessObject [{0}]. Exception : {1}",
+                        businessObject,
+                        e.getMessage()));
             }
-        });
+        }
+
+        return finderEntityDtos;
     }
 
     /**
