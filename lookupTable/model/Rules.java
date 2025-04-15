de11101250e80b05c61183c38f6e57e425b3b3c3
couroumah <ousmane.couroumah@gmail.com>
2025-04-15 12:09:53 +1000

    Create Rules.java

---
+++ lookupTable/model/Rules.java
@@ -0,0 +1,24 @@
+package com.westpac.murex.devops.lookupTable.model;
+
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+import java.util.Collection;
+
+@XmlRootElement(name = "rules")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Rules {
+    @XmlElement(name = "rule")
+    private Collection<Rule> ruleCollection;
+
+    public Collection<Rule> getRuleCollection() {
+        return ruleCollection;
+    }
+
+    public void setRuleCollection(Collection<Rule> ruleCollection) {
+        this.ruleCollection = ruleCollection;
+    }
+}
