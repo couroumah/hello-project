dc5ec83616367235a2b11545bf29fdf38aa6bb05
couroumah <ousmane.couroumah@gmail.com>
2025-04-15 11:59:19 +1000

    Create Assignment.java

---
+++ lookupTable/model/Assignment.java
@@ -0,0 +1,32 @@
+package com.westpac.murex.devops.lookupTable.model;
+
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "assignment")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Assignment {
+    @XmlElement(name = "fieldLabel")
+    private String fieldLabel;
+    @XmlElement(name = "fieldValue")
+    private String fieldValue;
+
+    public String getFieldLabel() {
+        return fieldLabel;
+    }
+
+    public void setFieldLabel(String fieldLabel) {
+        this.fieldLabel = fieldLabel;
+    }
+
+    public String getFieldValue() {
+        return fieldValue;
+    }
+
+    public void setFieldValue(String fieldValue) {
+        this.fieldValue = fieldValue;
+    }
+}
