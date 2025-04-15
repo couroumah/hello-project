f24985ef48cda3dd8b8c5ec0b9dcf04cf9ece8cf
couroumah <ousmane.couroumah@gmail.com>
2025-04-15 12:16:10 +1000

    Create ForegroundColor.java

---
+++ murex/model/ForegroundColor.java
@@ -0,0 +1,42 @@
+package com.westpac.murex.devops.model;
+
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "foregroundColor")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ForegroundColor {
+    @XmlElement(name = "fm")
+    private int red;
+    @XmlElement(name = "green")
+    private int green;
+    @XmlElement(name = "blue")
+    private int blue;
+
+    public int getRed() {
+        return red;
+    }
+
+    public void setRed(int red) {
+        this.red = red;
+    }
+
+    public int getGreen() {
+        return green;
+    }
+
+    public void setGreen(int green) {
+        this.green = green;
+    }
+
+    public int getBlue() {
+        return blue;
+    }
+
+    public void setBlue(int blue) {
+        this.blue = blue;
+    }
+}
