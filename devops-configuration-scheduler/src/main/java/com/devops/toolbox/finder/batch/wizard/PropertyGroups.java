f6d848072bfe10fdc1ac0ecfa7a172fabdf075d1
couroumah <ousmane.couroumah@gmail.com>
2025-08-11 15:27:54 +1000

    Update PropertyGroups.java

--- devops-configuration-scheduler/src/main/java/com/devops/toolbox/finder/batch/wizard/PropertyGroups.java
+++ devops-configuration-scheduler/src/main/java/com/devops/toolbox/finder/batch/wizard/PropertyGroups.java
@@ -1,15 +1,15 @@
 package com.devops.toolbox.finder.batch.wizard;
 
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
-import jakarta.xml.bind.annotation.XmlRootElement;
-import jakarta.xml.bind.annotation.XmlValue;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@XmlRootElement(name = "propertyGroups")
-@XmlAccessorType(XmlAccessType.FIELD)
-@Getter @Setter
+import java.util.List;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PropertyGroups {
     private List<PropertyGroup> propertyGroups;
 }
+
