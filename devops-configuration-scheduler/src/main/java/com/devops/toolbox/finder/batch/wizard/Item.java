4e50ffddbb683d08ac2fce7717c1287fa88d38ab
couroumah <ousmane.couroumah@gmail.com>
2025-08-11 15:30:03 +1000

    Create Item.java

---
+++ devops-configuration-scheduler/src/main/java/com/devops/toolbox/finder/batch/wizard/Item.java
@@ -0,0 +1,20 @@
+package com.devops.toolbox.finder.batch.wizard;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item {
+    @JsonProperty
+    private String key;
+    @JsonProperty
+    private String value;
+    @JsonProperty
+    private String type;
+    @JsonProperty
+    private String description;
+}
